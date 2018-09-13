a207f1cd8af9f8f7ef9ae1b57268b1f1a4f05287
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-09-13 18:16:38 +0800

    extract theme

--- src/utils/getPageContext.ts
+++ src/utils/getPageContext.ts
@@ -1,26 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 
 import { SheetsRegistry } from 'jss';
-import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+import { createGenerateClassName } from '@material-ui/core/styles';
+import theme from '../themes/default';
 
 // A theme with custom primary and secondary color.
 // It's optional.
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
-    },
-    secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700],
-    },
-  },
-});
 
 function createPageContext() {
   return {
