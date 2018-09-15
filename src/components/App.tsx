604b930f9565794b48e8e8399b7275b282ebf9df
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-09-15 11:06:14 +0800

    add styles to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,17 +2,36 @@ import * as React from 'react';
 import Helmet from 'react-helmet';
 import Head from './Head';
 import Header from './Header';
+import Footer from './Footer';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { withStyles, createStyles } from "@material-ui/core/styles";
 
 interface Props {
   title: string
+  classes: any
 }
 
-const App: React.SFC<Props> = ({ title, children }) => (
-  <div>
+const styles = createStyles({
+  layout: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    flex: 1
+  }
+});
+
+const App: React.SFC<Props> = ({ title, children, classes }) => (
+  <div className={classes.layout}>
+    <CssBaseline/>
     <Head title={title}/>
     <Header/>
-    {children}
+    <main className={classes.main}>
+      {children}
+    </main>
+    <Footer/>
   </div>
 );
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface Props {
+}
+
+const Footer: React.SFC<Props> = () => (
+  <footer>
+    Footer
+  </footer>
+);
+
+export default Footer;
