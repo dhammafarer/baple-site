1dbc242db9290fbbcfe3ab37e9e9441378b1ac68
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-09-16 22:59:18 +0800

    create UnderConstruction

---
+++ src/pages/en/plastics/bombas-de-locion.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import UnderConstruction from '../../../components/pages/UnderConstruction';
+
+interface Props {
+}
+
+class BombasDeLocionEn extends React.Component<Props, {}> {
+  render () {
+    return (
+      <UnderConstruction
+        lang={'en'}
+      />
+    );
+  }
+}
+
+export default BombasDeLocionEn;
