5daa6188c7ae0c452d085b95820b303f8e2d6d77
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-09-16 21:28:22 +0800

    add contact to nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,6 +6,8 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
 import { createStyles, withStyles } from "@material-ui/core/styles";
 import Link from './Link';
 import CloseIcon from "@material-ui/icons/Close";
@@ -23,26 +25,45 @@ interface Props {
   }
 }
 
-const styles = createStyles({
+const styles = (theme:any) => createStyles({
   list: {
+    height: '100%',
     width: 300,
     display: 'flex',
     flexDirection: 'column',
     textAlign: 'center',
     marginTop: '1.5em',
+    justifyContent: 'space-between',
   },
   logo: {
     width: 150,
     marginBottom: 20,
-  },
-  langs: {
-    marginTop: '1em'
   },
   close: {
     alignSelf: 'flex-end',
     position: 'absolute',
     top: 4,
     right: 4,
+  },
+  contact: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    alignSelf: 'center',
+    width: '100%',
+    background: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  details: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    marginRight: '0.4em'
   }
 });
 
@@ -53,31 +74,44 @@ const Nav: React.SFC<Props> = ({ open, handleClose, nav, classes, lang }) => (
       role="button"
       onClick={handleClose}
       onKeyDown={handleClose}
+      className={classes.list}
     >
-      <div className={classes.list}>
-        <IconButton className={classes.close}>
-          <CloseIcon color="secondary"/>
-        </IconButton>
-        <List>
-          <img className={classes.logo} src={nav.logo.childImageSharp.sizes.src}/>
-          <Typography variant="title" gutterBottom={true}>{nav.title}</Typography>
-          {
-            nav.items.map((xs:any) => xs.map((x:any) =>
-              <Link to={x.to} lang={lang}>
-                <ListItem button={true} divider={true} style={{justifyContent: "center"}}>
-                  <Typography
-                    variant="caption"
-                    className={classes.item}
-                    color="secondary"
-                    align="center"
-                  >
-                    {x.label}
-                  </Typography>
-                </ListItem>
-              </Link>
-            ))
-          }
-        </List>
+      <IconButton className={classes.close}>
+        <CloseIcon color="secondary"/>
+      </IconButton>
+      <List>
+        <img className={classes.logo} src={nav.logo.childImageSharp.sizes.src}/>
+        <Typography variant="title" gutterBottom={true}>{nav.title}</Typography>
+        {
+          nav.items.map((xs:any) => xs.map((x:any) =>
+            <Link to={x.to} lang={lang}>
+              <ListItem button={true} divider={true} style={{justifyContent: "center"}}>
+                <Typography
+                  variant="caption"
+                  className={classes.item}
+                  color="secondary"
+                  align="center"
+                >
+                  {x.label}
+                </Typography>
+              </ListItem>
+            </Link>
+          ))
+        }
+      </List>
+      <div className={classes.contact}>
+        <div className={classes.details}>
+          <PhoneIcon className={classes.icon}/>
+          <Typography color="inherit" gutterBottom>
+            +886-2-5431-2421
+          </Typography>
+        </div>
+        <div className={classes.details}>
+          <EmailIcon className={classes.icon}/>
+          <Typography color="inherit">
+            info@baple.es
+          </Typography>
+        </div>
       </div>
     </div>
   </Drawer>
